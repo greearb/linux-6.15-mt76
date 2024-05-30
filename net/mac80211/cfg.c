9feebc8fe2923b012a2c1f8360c52f635a4d7d44
Howard Hsu <howard-yh.hsu@mediatek.com>
2024-05-30 16:08:54 +0800

    mtk: mac80211: add new argument link_id in set_bitrate_mask
    mt76 driver needs link id to fix parital rate on the specific link.
    
    Signed-off-by: Howard Hsu <howard-yh.hsu@mediatek.com>

--- net/mac80211/cfg.c
+++ net/mac80211/cfg.c
@@ -3499,7 +3499,7 @@ static int ieee80211_set_bitrate_mask(struct wiphy *wiphy,
 	}
 
 	if (ieee80211_hw_check(&local->hw, HAS_RATE_CONTROL)) {
-		ret = drv_set_bitrate_mask(local, sdata, mask);
+		ret = drv_set_bitrate_mask(local, sdata, mask, link_id);
 		if (ret)
 			return ret;
 	}
